72060b03df5b529c091da127a6bfd68bf8b93cf9
vigneshrt521-rgb <vignesh.rt.521@gmail.com>
2026-06-25 10:50:43 +0530

    Vignesh first commit

---
+++ src/test/java/com/example/InventoryManagementSystem/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InventoryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
